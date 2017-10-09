34f87645ae623551cd69537fa6dfd21351b847dc
hyunuk <nnhope@hotmail.com>
2017-10-09 16:41:49 +0900

    Add Chapter16 vs2017

---
+++ VS2017/Chapter16/Ch16_ImageEditorSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ch16_ImageEditorSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
