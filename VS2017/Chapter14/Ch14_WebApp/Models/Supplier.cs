36e58215323151f3dc03ae869f767ffb73cd2b7b
hyunuk <nnhope@hotmail.com>
2017-10-09 16:38:38 +0900

    Add Chapter14 vs2017

---
+++ VS2017/Chapter14/Ch14_WebApp/Models/Supplier.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Packt.CS7
+{
+    [Table("Suppliers")]
+    public class Supplier
+    {
+        public int SupplierID { get; set; }
+        [Required]
+        [StringLength(15)]
+        [Display(Name = "회사 이름")]
+        public string CompanyName { get; set; }
+    }
+}
