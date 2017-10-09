c5c7d77436836361e4b1e8c019f7fe2f4dbddb1d
Kim Hyunuk <hyun@Kimui-MacBook-Pro.local>
2017-10-09 19:53:58 +0900

    Add Chapter14 vscode

---
+++ VSCode/Chapter14/Ch14_WebApp/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ch14_WebApp.Models;
+using Packt.CS7;
+
+namespace Ch14_WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Northwind db;
+        public HomeController(Northwind injectedContext)
+        {
+            db = injectedContext;
+        }
+        public IActionResult Index()
+        {
+            var model = new HomeIndexViewModel
+            {
+                VisitorCount = (new Random()).Next(1, 1001),
+                Products = db.Products.ToArray()
+            };
+            return View(model);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
