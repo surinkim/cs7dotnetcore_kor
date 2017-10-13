bdde165928aedc88688c82aad619527e82b7fa6d
Kim Hyunuk <hyun@Kimui-MacBook-Pro.local>
2017-10-13 20:53:17 +0900

    Add Ch03, 04 vscode

---
+++ VSCode/Chapter03/Ch03_SelectionStatements/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+using System;
+using static System.Console;
+
+namespace Ch03_SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                WriteLine("There are no arguments.");
+            }
+            else
+            {
+                WriteLine("There is at least one argument.");
+            }
+
+            object o = 3;
+            int j = 4;
+
+            if (o is int i)
+            {
+                WriteLine($"{i} x {j} = {i * j}");
+            }
+            else
+            {
+                WriteLine("o is not an int so it cannot multiply!");
+            }
+
+            A_label:
+            var number = (new Random()).Next(1, 7);
+            WriteLine($"My random number is {number}");
+            switch (number)
+            {
+                case 1:
+                    WriteLine("One");
+                    break; // switch문의 끝으로 점프한다. 
+                case 2:
+                    WriteLine("Two");
+                    goto case 1;
+                case 3:
+                case 4:
+                    WriteLine("Three or four");
+                    goto case 1;
+                case 5:
+                    // 0.5초 동안 대기한다. 
+                    System.Threading.Thread.Sleep(500);
+                    goto A_label;
+                default:
+                    WriteLine("Default");
+                    break;
+            } // switch문의 끝
+
+            string path = "/Users/hyun/Code/Chapter03"; // macOS 
+            //string path = @"C:\Code\Chapter03"; // Windows 
+
+            Stream s = File.Open(
+              Path.Combine(path, "file.txt"),
+              FileMode.OpenOrCreate);
+
+            switch (s)
+            {
+                case FileStream writeableFile when s.CanWrite:
+                    WriteLine("The stream is to a file that I can write to.");
+                    break;
+                case FileStream readOnlyFile:
+                    WriteLine("The stream is to a read-only file.");
+                    break;
+                case MemoryStream ms:
+                    WriteLine("The stream is to a memory address.");
+                    break;
+                default: // always evaluated last despite its current position 
+                    WriteLine("The stream is some other type.");
+                    break;
+                case null:
+                    WriteLine("The stream is null.");
+                    break;
+            }
+
+
+
+        }
+    }
+}
