6fdab3c055b06043b5c496609eb6072f708e4906
Kim Hyunuk <hyun@Kimui-MacBook-Pro.local>
2017-10-15 13:49:36 +0900

    Add Chapter05 VSCode

---
+++ VSCode/Chapter05/Ch05_Debugging/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace Ch05_Debugging
+{
+    class Program
+    {
+        static double Add(double a, double b)
+        {
+            return a + b; // 의도적으로 버그를 만들었다.
+        }
+
+        static void Main(string[] args)
+        {
+            double a = 4.5; // var를 사용해도 좋다.
+            double b = 2.5;
+            double answer = Add(a, b);
+            WriteLine($"{a} + {b} = {answer}");
+            ReadLine(); //사용자가 ENTER를 누를 때까지 대기한다.
+        }
+    }
+}
