f3e103fd3cc131b6f47bc18cb7ed021e2bb5bfcb
Kim Hyunuk <hyun@Kimui-MacBook-Pro.local>
2017-10-16 13:25:50 +0900

    Add Chapter10 VSCode

---
+++ VSCode/Chapter10/Ch10_FileSystem/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+using static System.IO.Directory;
+
+
+namespace Ch10_FileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 디렉토리 경로를 정의한다.
+            //string dir = @"C:\Code\Ch10_Example"; // 윈도우 
+            string dir = @"/Users/hyun/Code/Ch10_Example/"; // 맥OS
+
+            // 디렉토리가 존재하는지 확인한다.
+            WriteLine($"Does {dir} exist? {Exists(dir)}");
+            // 디렉토리를 생성한다. 
+            CreateDirectory(dir);
+            WriteLine($"Does {dir} exist? {Exists(dir)}");
+            // 디렉토리를 삭제한다.
+            Delete(dir);
+            WriteLine($"Does {dir} exist? {Exists(dir)}");
+
+            //string textFile = @"C:\Code\Ch10.txt"; // Windows 
+            //string backupFile = @"C:\Code\Ch10.bak"; // Windows 
+            string textFile = @"/Users/hyun/Code/Ch10.txt"; // 맥OS 
+            string backupFile = @"/Users/hyun/Code/Ch10.bak"; // 맥OS 
+
+            // 파일이 존재하는지 확인한다. 
+            WriteLine($"Does {textFile} exist? {File.Exists(textFile)}");
+
+            // 파일을 생성하고 파일 안에 한 줄을 입력한다. 
+            StreamWriter textWriter = File.CreateText(textFile);
+            textWriter.WriteLine("Hello, C#!");
+            textWriter.Dispose();
+            WriteLine($"Does {textFile} exist? {File.Exists(textFile)}");
+
+            // 파일을 복사하고 파일이 이미 존재한다면 덮어쓴다. 
+            File.Copy(textFile, backupFile, true);
+            WriteLine($"Does {backupFile} exist? {File.Exists(backupFile)}");
+
+            // 파일 삭제 
+            File.Delete(textFile);
+            WriteLine($"Does {textFile} exist? {File.Exists(textFile)}");
+
+            // 텍스트 파일 읽기 
+            StreamReader textReader = File.OpenText(backupFile);
+            WriteLine(textReader.ReadToEnd());
+            textReader.Dispose();
+
+
+            WriteLine($"File Name: {Path.GetFileName(textFile)}");
+            WriteLine($"File Name without Extension:{ Path.GetFileNameWithoutExtension(textFile)}"); 
+            WriteLine($"File Extension: {Path.GetExtension(textFile)}");
+            WriteLine($"Random File Name: {Path.GetRandomFileName()}");
+            WriteLine($"Temporary File Name: {Path.GetTempFileName()}");
+
+            string backup = @"/Users/hyun/Code/Ch10.bak"; // 맥OS 
+            //string backup = @"C:\Code\Ch10.bak"; // 윈도우 
+            var info = new FileInfo(backup);
+            WriteLine($"{backup} contains {info.Length} bytes.");
+            WriteLine($"{backup} was last accessed {info.LastAccessTime}.");
+            WriteLine($"{backup} has readonly set to {info.IsReadOnly}.");
+
+
+
+        }
+    }
+}
---
+++ VSCode/Chapter10/Ch10_Streams/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using static System.Console;
+using System.IO.Compression;
+
+namespace Ch10_Streams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string 배열을 정의한다.
+            string[] callsigns = new string[] { "Husker", "Starbuck", "Apollo", "Boomer", "Bulldog", "Athena", "Helo", "Racetrack" };
+
+            // 텍스트 쓰기 헬퍼를 사용하여 쓸 파일을 정의한다.
+            string textFile = @"/Users/hyun/Code/Ch10_Streams.txt";
+            //string textFile = @"C:\Code\Ch10_Streams.txt"; // 윈도우 
+            StreamWriter text = File.CreateText(textFile);
+
+            // string 배열의 각 항목을 스트림에 쓴다.
+            foreach (string item in callsigns)
+            {
+                text.WriteLine(item);
+            }
+            text.Dispose(); // 스트림을 닫는다. 
+
+            // 파일의 내용을 콘솔에 출력한다.
+            WriteLine($"{textFile} contains {new FileInfo(textFile).Length} bytes."); 
+            WriteLine(File.ReadAllText(textFile));
+
+            // XML 쓰기 헬퍼를 사용하여 쓸 파일을 정의한다.
+            string xmlFile = @"/Users/hyun/Code/Ch10_Streams.xml";
+            //string xmlFile = @"C:\Code\Ch10_Streams.xml"; 
+
+            FileStream xmlFileStream = File.Create(xmlFile);
+            XmlWriter xml = XmlWriter.Create(xmlFileStream,
+              new XmlWriterSettings { Indent = true });
+
+            // 파일에 XML 선언부를 쓴다.
+            xml.WriteStartDocument();
+
+            // root 엘리먼트를 쓴다.
+            xml.WriteStartElement("callsigns");
+
+            // string 배열의 각 항목을 스트림에 쓴다.
+            foreach (string item in callsigns)
+            {
+                xml.WriteElementString("callsign", item);
+            }
+
+            // root 엘리먼트를 닫는다.
+            xml.WriteEndElement();
+            xml.Dispose();
+            xmlFileStream.Dispose();
+
+            // 파일의 내용을 콘솔에 출력한다.
+            WriteLine($"{xmlFile} contains {new FileInfo(xmlFile).Length} bytes."); 
+            WriteLine(File.ReadAllText(xmlFile));
+
+
+            // XML 출력을 압축한다.
+            string gzipFilePath = @"/Users/hyun/Code/Ch10.gzip";
+            //string gzipFilePath = @"C:\Code\Ch10.gzip"; // 윈도우 
+
+            FileStream gzipFile = File.Create(gzipFilePath);
+            GZipStream compressor = new GZipStream(gzipFile,
+            CompressionMode.Compress);
+            XmlWriter xmlGzip = XmlWriter.Create(compressor);
+            xmlGzip.WriteStartDocument();
+            xmlGzip.WriteStartElement("callsigns");
+            foreach (string item in callsigns)
+            {
+                xmlGzip.WriteElementString("callsign", item);
+            }
+            xmlGzip.Dispose();
+            compressor.Dispose(); // also closes the underlying stream 
+
+            // 압축 파일의 내용을 콘솔에 출력한다.
+            WriteLine($"{gzipFilePath} contains {new FileInfo(gzipFilePath).Length} bytes."); 
+            WriteLine(File.ReadAllText(gzipFilePath));
+
+            // 압축 파일을 읽는다. 
+            WriteLine("Reading the compressed XML file:");
+                    gzipFile = File.Open(gzipFilePath, FileMode.Open); 
+            GZipStream decompressor = new GZipStream(gzipFile,
+            CompressionMode.Decompress);
+                    XmlReader reader = XmlReader.Create(decompressor); 
+            while (reader.Read()) 
+            { 
+            // callsign 엘리먼트인지 확인한다.
+              if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "callsign")) 
+              { 
+                reader.Read(); // 엘리먼트 안의 텍스트 노드로 이동한다. 
+                WriteLine($"{reader.Value}"); // 값을 읽는다. 
+                }
+            }
+            reader.Dispose(); 
+            decompressor.Dispose();
+        }
+    }
+}
